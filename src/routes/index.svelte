38d4b00f67aceb3fe152f113a1263c17d6ee3f53
Mayc Rodrigues <maycrodriguess@gmail.com>
2022-01-19 14:32:27 -0300

    [feat] adicionando pagina inicial

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const response = await fetch('data.json');
+		const data = await response.json();
+		return {
+			props: {
+				data
+			}
+		};
+	};
+</script>
+
+<script>
+  import SvelteSeo from "svelte-seo";
+
+	export let data;
+</script>
+
+<SvelteSeo
+  title={data.title} description={data.description}
+
+  openGraph={{
+    title: data.title,
+    description: data.description,
+    url: data.url,
+    type: 'website',
+    images: [
+      {
+        url: data.image.url,
+        width: data.image.width,
+        height: data.image.height,
+        alt: data.title
+      }
+     ]
+  }}
+
+  twitter={{
+    site: data.twitter.username,
+    title: data.title,
+    description: data.description,
+    image: data.image.url,
+    imageAlt: data.image.alt,
+  }}
+
+  jsonLd={{
+    '@type': 'Article',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': data.url
+    },
+    headline: data.description,
+    image: [data.image.url],
+    datePublished: '2020-08-03T17:31:37Z',
+    dateModified: '2020-08-20T09:31:37Z',
+    author: {
+      '@type': 'Person',
+      name: data.author
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: data.organization,
+      logo: {
+        '@type': 'ImageObject',
+        url: data.logo.url
+      }
+    }
+  }}
+
+/>
+
+<div class="project">
+  {#if data}
+    <div class="flex justify-center items-center min-h-screen">
+      <div class="text-center p-8">
+        <h1 class="uppercase">{data.title}</h1>
+        <h3>{data.description}</h3>
+      </div>
+    </div>  
+  {/if}
+</div>
