8e0b2af758d63746f5e654db80c02e769e794650
atelielacofino <keyllafarina@gmail.com>
2022-01-28 14:46:30 -0300

    chore: atualizando gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ publish(
  'build', // path to public directory
  {
     branch: 'gh-pages',
-    repo: 'https://github.com/maycrodrigues/atelie-laco-fino.git', // Update to point to your repository
+    repo: 'https://github.com/atelielacofino/atelielacofino.github.io.git', // Update to point to your repository
     user: {
-      name: 'Mayc Rodrigues', // update to use your name
-      email: 'maycrodriguess@gmail.com' // Update to use your email
+      name: 'Keylla Farina', // update to use your name
+      email: 'keyllafarina@gmail.com' // Update to use your email
     },
     dotfiles: true
   }, () => { console.log('Deploy Complete!'); }
